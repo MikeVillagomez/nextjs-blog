8c21c567ff1830ebdf73fb222d36f9d45808f330
MikeVillagomez <Mbv23@njit.edu>
2023-11-13 04:33:58 -0500

    cvgh

---
+++ pages/index.js
@@ -0,0 +1,47 @@
+import Head from 'next/head';
+import Layout, { siteTitle } from '../components/layout';
+import utilStyles from '../styles/utils.module.css';
+import { getSortedPostsData } from '../lib/posts';
+import Link from 'next/link';
+import Date from '../components/date';
+
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData,
+    },
+  };
+}
+
+export default function Home({ allPostsData }) {
+  return (
+    <Layout home>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+      <section className={utilStyles.headingMd}>
+        <p>Hello, I'm Mike Villagomez, a passionate web developer from Union City, New Jersey. I am currently pursuing a Bachelor of Business and Information Systems at the New Jersey Institute of Technology, where I'm honing my technical skills and grounding them in solid business principles. My journey in tech began in my hometown, and I'm eager to leverage my education to build innovative web solutions that drive business success.</p>
+        <p>
+          you can contact me on{' '}
+          <a href="https://twitter.com/mikev2002">Twitter</a>.
+        </p>
+      </section>
+
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+            <Link href={`/posts/${id}`}>{title}</Link>
+            <br />
+            <small className={utilStyles.lightText}>
+              <Date dateString={date} />
+            </small>
+          </li>
+          ))}
+        </ul>
+      </section>
+    </Layout>
+  );
+}
